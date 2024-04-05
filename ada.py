a95e3d34d4fb3997edf705c1a158bd4642e8dfd3
Iri.Pulenta <162722735+IriPulenta@users.noreply.github.com>
2024-04-04 22:58:23 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    upper_name = (first_name+" "+last_name).upper()
+    lower_name = (first_name+""+last_name).lower()
+    
+    print(first_name+""+last_name)
+    print(upper_name)
+    print(lower_name)
