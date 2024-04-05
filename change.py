f8b02a373f068ea89fff190c07865b7cfcbce14c
Iri.Pulenta <162722735+IriPulenta@users.noreply.github.com>
2024-04-04 23:15:51 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,9 @@
 def change():
     expense = 23.75
     money = 100
+    gastos=float(input(23.75))
+    pagos=float(intput(100)
+                devolucion = pago - gasto
+    print(cantidad a devolver: $, devolucion)
+    
+    
